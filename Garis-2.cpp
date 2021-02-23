1359c8f97d10cdb9fc23f4c1e0725522f2e60369
Ni Made Widiasanti <56221398+nmwsanti@users.noreply.github.com>
2021-02-23 19:21:14 +0700

    Add files via upload

---
+++ Garis-2.cpp
@@ -0,0 +1,26 @@
+#include <GL/glut.h>
+#include <math.h>
+
+void display(void)
+{
+glClearColor (0.0f, 0.0f, 0.0f, 0.0f);
+glClear (GL_COLOR_BUFFER_BIT);
+glPushMatrix ();
+glColor3f(1,1,1);
+glBegin(GL_LINES);
+glVertex3f(0.10,10.0,13.13);
+glVertex3f(0,0,0.0);
+glEnd (); 
+glFlush();
+}
+
+int main(int argc, char **argv)
+{
+glutInit(&argc, argv);
+glutCreateWindow("GARIS");
+glutDisplayFunc(display);
+glutMainLoop();
+return 0;
+}
+
+/* Ni Made Widiasanti / 19051397002 / D4 Manajemen Informatika 2019A / Grafika Komputer */
